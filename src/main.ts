40cbad0bfb18503b355756753ecbc784a66f4252
Jorgenettodev <jorgenetto96@gmail.com>
2024-10-22 05:41:29 -0300

    Log a message when the server is running

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  config();
+  await app.listen(process.env.PORT, async () => {
+    // const url = await app.getUrl();
+    console.log(`Server is running on: http://localhost:${process.env.PORT}`);
+  });
 }
 bootstrap();
