f070ffbcc638a8fa4debf107cfca80e57e2a5150
Jorgenettodev <jorgenetto96@gmail.com>
2024-10-22 06:12:15 -0300

    Create route post Create route get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  standard(): string {
+    return 'standard get';
+  }
+
+  @Get(':nome')
+  helloNome(@Param('nome') nome: string): string {
+    return `Olá ${nome}`;
+  }
+
+  // crie um metodo post que no body contenha nome e mensagem. Retorne na tela o nome e mensagem.
+  @Post()
+  message(@Body() body: { name: string; message: string }): string {
+    return `
+    Nome: ${body.name}
+    Mensagem: ${body.message}
+    `;
   }
 }
